ab073f70e768c0068301ff644350af015ba7cc28
blau3 <borisgambo1626@gmail.com>
2025-12-24 18:15:08 +0300

    commit 6

--- src/djangoproject/blog_app/models.py
+++ src/djangoproject/blog_app/models.py
@@ -1,8 +1,13 @@
 from django.contrib.auth.models import User
 from django.db import models
+
+
 class Category(models.Model):
     title = models.CharField(max_length=100)
     slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.title
 
 
 class Post(models.Model):
@@ -10,6 +15,20 @@ class Post(models.Model):
     slug = models.SlugField(unique=True)
     content = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name="posts"
+    )
     published = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    views_count = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+
+    def increase_views_count(self):
+        self.views_count += 1
+        self.save(update_fields=["views_count"])
